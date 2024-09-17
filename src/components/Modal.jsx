bb11dfaf2a82925121a81a42bbdc762314accb75
Gosia Magdzik <melassa26@gmail.com>
2024-09-17 12:01:25 +0200

    nested animations

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,10 +5,14 @@ export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <motion.dialog 
-        initial={{  opacity: 0, y: 30 }}
-        animate={{  opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 30 }}
+      <motion.dialog
+        variants={{
+          hidden: { opacity: 0, y: 30 },
+          visible: { opacity: 0.9, y: 30 }
+        }} 
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
         open 
         className="modal">
         <h2>{title}</h2>
